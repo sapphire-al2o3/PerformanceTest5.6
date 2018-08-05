f677d69f75b0973d2e78787238abd1041b276a62
sapphire-al2o3 <t_telkun@hotmail.com>
2018-08-05 15:46:17 +0900

    Unity5.6のパフォーマンス計測コード

---
+++ Assets/CoroutineTest/CoroutineTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineTest : MonoBehaviour {
+
+    IEnumerator Test0()
+    {
+        while (true)
+        {
+            yield return null;
+        }
+    }
+
+    IEnumerator Test1()
+    {
+        while (true)
+        {
+            yield return 0f;
+        }
+    }
+
+	void Start()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            StartCoroutine(Test0());
+        }
+	}
+}
