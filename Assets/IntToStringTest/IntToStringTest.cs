3ae47f1859a57f47f33dd5dc8a086f0b14e8e795
sapphire-al2o3 <t_telkun@hotmail.com>
2018-08-10 12:49:59 +0900

    整数の文字列変換のテストを追加

---
+++ Assets/IntToStringTest/IntToStringTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+using System.Text;
+
+public class IntToStringTest : MonoBehaviour
+{
+	int count0 = 0;
+	string countText0 = "";
+
+	int count1 = 0;
+	string countText1 = "";
+
+	StringBuilder sb = new StringBuilder();
+
+	void Update()
+	{
+		// 32byte
+		Profiler.BeginSample("ToString");
+		countText0 = count0.ToString();
+		count0++;
+		Profiler.EndSample();
+
+		// 64byte
+		Profiler.BeginSample("StringBuilder");
+		sb.Length = 0;
+		sb.Append(count1);
+		countText1 = sb.ToString();
+		count1++;
+		Profiler.EndSample();
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label(countText0);
+		GUILayout.Label(countText1);
+	}
+}
