09d57455b346ae08ce8622cc01afb1a6fbb83132
sapphire-al2o3 <t_telkun@hotmail.com>
2018-08-20 14:36:35 +0900

    DictionaryのGC Allocテスト追加

---
+++ Assets/DictionaryTest/DictionaryTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class DictionaryTest : MonoBehaviour
+{
+	void Start()
+	{
+		Dictionary<int, int> dic = new Dictionary<int, int>();
+
+		for (int i = 0; i < 10000; i++)
+		{
+			dic.Add(i, i);
+		}
+
+		int sum = 0;
+
+		// 88byte
+		Profiler.BeginSample("values");
+		foreach (var e in dic.Values)
+		{
+			sum += e;
+		}
+		Profiler.EndSample();
+
+		// 88byte
+		Profiler.BeginSample("keys");
+		foreach (var k in dic.Keys)
+		{
+			sum += dic[k];
+		}
+		Profiler.EndSample();
+
+		// 64byte
+		Profiler.BeginSample("pair");
+		foreach (var pair in dic)
+		{
+			sum += pair.Value;
+		}
+		Profiler.EndSample();
+	}
+}
