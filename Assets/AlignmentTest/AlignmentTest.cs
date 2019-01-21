c0d0ba57b395f8b09a751204f4b1121934aedd51
sapphire-al2o3 <t_telkun@hotmail.com>
2019-01-21 14:50:42 +0900

    クラスのサイズ計測

---
+++ Assets/AlignmentTest/AlignmentTest.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class AlignmentTest : MonoBehaviour
+{
+	class A
+	{
+		string a;
+		byte b;
+		string c;
+		byte d;
+	}
+
+	// 40byte
+	class B
+	{
+		string a;
+		string b;
+		byte c;
+		byte d;
+	}
+
+	// 40byte
+	class B1
+	{
+		string a;
+		string b;
+		byte c;
+	}
+
+	// 16byte
+	class Empty
+	{
+	}
+
+	// 24byte
+	class A1
+	{
+		string a;
+	}
+
+	// 17byte
+	class A2
+	{
+		byte a;
+	}
+
+	// 18byte
+	class A3
+	{
+		byte a;
+		byte b;
+	}
+
+	// 32byte
+	class A4
+	{
+		byte a;
+		string b;
+	}
+
+	// 32byte
+	class A5
+	{
+		string b;
+		int a;
+	}
+
+	// 32byte
+	class A6
+	{
+		string b;
+		string a;
+	}
+
+	void Start()
+	{
+		Profiler.BeginSample("A");
+		var a = new A6();
+		Profiler.EndSample();
+
+		Profiler.BeginSample("B");
+		B b = new B();
+		Profiler.EndSample();
+	}
+}
