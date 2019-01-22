66a071755280147c802909b25301dfd00e1c2494
sapphire-al2o3 <t_telkun@hotmail.com>
2019-01-23 02:30:03 +0900

    keyとvalueの型をstringでテスト

--- Assets/DictionaryTest/DictionaryTest.cs
+++ Assets/DictionaryTest/DictionaryTest.cs
@@ -5,76 +5,98 @@ using UnityEngine.Profiling;
 
 public enum EnumType
 {
-	A,
-	B,
-	C,
-	D,
-	E
+    A,
+    B,
+    C,
+    D,
+    E
 }
 
 public class EnumTypeComparer : IEqualityComparer<EnumType>
 {
-	public bool Equals( EnumType x, EnumType y )
-	{
-		return x == y;
-	}
+    public bool Equals(EnumType x, EnumType y)
+    {
+        return x == y;
+    }
 
-	public int GetHashCode( EnumType obj )
-	{
-		return ( int )obj;
-	}
+    public int GetHashCode(EnumType obj)
+    {
+        return (int)obj;
+    }
 }
 
 public class DictionaryTest : MonoBehaviour
 {
 
-	void Start()
-	{
-		Dictionary<int, int> dic = new Dictionary<int, int>();
+    void Start()
+    {
+        Dictionary<int, int> dic = new Dictionary<int, int>();
 
-		for (int i = 0; i < 10000; i++)
-		{
-			dic.Add(i, i);
-		}
+        for (int i = 0; i < 10000; i++)
+        {
+            dic.Add(i, i);
+        }
 
-		int sum = 0;
+        int sum = 0;
 
-		// 88byte
-		Profiler.BeginSample("values");
-		foreach (var e in dic.Values)
-		{
-			sum += e;
-		}
-		Profiler.EndSample();
+        // 88byte
+        Profiler.BeginSample("values");
+        foreach (var e in dic.Values)
+        {
+            sum += e;
+        }
+        Profiler.EndSample();
 
-		// 88byte
-		Profiler.BeginSample("keys");
-		foreach (var k in dic.Keys)
-		{
-			sum += dic[k];
-		}
-		Profiler.EndSample();
+        // 88byte
+        Profiler.BeginSample("keys");
+        foreach (var k in dic.Keys)
+        {
+            sum += dic[k];
+        }
+        Profiler.EndSample();
 
-		// 64byte
-		Profiler.BeginSample("pair");
-		foreach (var pair in dic)
-		{
-			sum += pair.Value;
-		}
-		Profiler.EndSample();
+        // 64byte
+        Profiler.BeginSample("pair");
+        foreach (var pair in dic)
+        {
+            sum += pair.Value;
+        }
+        Profiler.EndSample();
 
-		// 0.6KB
-		var comp = new EnumTypeComparer();
-		Profiler.BeginSample("dictionary<EnumType, int>");
-		Dictionary<EnumType, int> dic2 = new Dictionary<EnumType, int>(comp);
-		Profiler.EndSample();
+        // 0.5KB
+        var comp = new EnumTypeComparer();
+        Profiler.BeginSample("dictionary<EnumType, int>");
+        Dictionary<EnumType, int> dic2 = new Dictionary<EnumType, int>(comp);
+        Profiler.EndSample();
 
-		// 488byte
-		Profiler.BeginSample("dictionary<int, int>");
-		Dictionary<int, int> dic1 = new Dictionary<int, int>();
-		Profiler.EndSample();
+        // 488byte
+        Profiler.BeginSample("dictionary<int, int>");
+        Dictionary<int, int> dic1 = new Dictionary<int, int>();
+        Profiler.EndSample();
 
+        // 217.3KB
+        Profiler.BeginSample("dictionary<int, int>(10000)");
+        dic1 = new Dictionary<int, int>(10000);
+        Profiler.EndSample();
 
+        // 0.5KB
+        Profiler.BeginSample("dictionary<int, string>");
+        Dictionary<int, string> dic3 = new Dictionary<int, string>();
+        Profiler.EndSample();
 
-	}
+        // 0.5KB
+        Profiler.BeginSample("dictionary<string, int>");
+        var dicsi = new Dictionary<string, int>();
+        Profiler.EndSample();
+
+        // 0.6KB
+        Profiler.BeginSample("dictionary<string, string>");
+        Dictionary<string, string> dic4 = new Dictionary<string, string>();
+        Profiler.EndSample();
+
+        // 304.1KB
+        Profiler.BeginSample("dictionary<string, string>(10000)");
+        dic4 = new Dictionary<string, string>(10000);
+        Profiler.EndSample();
+    }
 }
