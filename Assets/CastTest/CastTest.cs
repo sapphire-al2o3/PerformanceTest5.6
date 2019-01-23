f3032bdbcba42c0d864aba7edbc9b0015b1849da
sapphire-al2o3 <t_telkun@hotmail.com>
2019-01-23 14:44:47 +0900

    Enumのキャストの計測追加

---
+++ Assets/CastTest/CastTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class CastTest : MonoBehaviour
+{
+	enum EnumNum
+	{
+		Num0,
+		Num1,
+		Num2,
+		Num3
+	}
+
+	void Start()
+	{
+		EnumNum n = EnumNum.Num3;
+		Profiler.BeginSample("cast () enum -> int");
+		int i = (int)n;
+		Profiler.EndSample();
+
+		// 40byte
+		Profiler.BeginSample("cast Convert.ToInt32 enum -> int");
+		i = System.Convert.ToInt32(n);
+		Profiler.EndSample();
+
+		// 40byte
+		Profiler.BeginSample("cast Enum.ToObject int -> enum");
+		n = (EnumNum)System.Enum.ToObject(typeof(EnumNum), i);
+		Profiler.EndSample();
+
+		// 40byte
+		Profiler.BeginSample("cast () int -> enum");
+		n = (EnumNum)i;
+		Profiler.EndSample();
+	}
+}
