2642a28b6df283bd75739ff04e0d63500969d2f2
sapphire-al2o3 <t_telkun@hotmail.com>
2019-01-31 14:17:35 +0900

    パーティクルのテスト追加

---
+++ Assets/ParticleTest/ParticleTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTest : MonoBehaviour {
+
+	[SerializeField]
+	GameObject ps;
+
+	void Start()
+	{
+		// trail使っていなくてもアクセスするとDefault-Materialがつく
+		var pr = ps.GetComponent<ParticleSystemRenderer>();
+		foreach (var m in pr.materials)
+		{
+			Debug.Log(m.name);
+		}
+	}
+}
